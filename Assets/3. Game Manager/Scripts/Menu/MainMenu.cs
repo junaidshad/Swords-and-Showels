e89710215d1b7833b0d5044ff8d7777963a51318
Junaid Shad <xadxunaid@gmail.com>
2018-10-07 19:33:05 +0500

    Transitioning Scenes
    1. Preparing to Transition
    2. Triggering Animations by Script
    3. Completing the Transition

---
+++ Assets/3. Game Manager/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+	
+	// Function that can recieve animation event
+	// Functions to play fade in/out
+
+	[SerializeField] Animation _mainMenuAnimator;
+	[SerializeField] AnimationClip _fadeOutAnimation;
+	[SerializeField] AnimationClip _fadeInAnimation;
+	
+
+	public void OnFadeOutComplete(){
+		Debug.LogWarning("Fade out Complete");
+
+	}
+	public void OnFadeInComplete(){
+		Debug.LogWarning("Fade in Complete");
+		UIManager.Instance.SetDummyCameraActive(true);
+
+	}
+
+	public void FadeIn(){
+		_mainMenuAnimator.Stop();
+		_mainMenuAnimator.clip = _fadeInAnimation;
+		_mainMenuAnimator.Play();
+	}
+	public void FadeOut(){
+		UIManager.Instance.SetDummyCameraActive(false);
+		_mainMenuAnimator.Stop();
+		_mainMenuAnimator.clip = _fadeOutAnimation;
+		_mainMenuAnimator.Play();
+	}
+}
---
+++ Assets/3. Game Manager/Scripts/Menu/UIManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : Singleton<UIManager> {
+	[SerializeField] MainMenu _mainMenu;
+	[SerializeField] Camera _dummyCamera;
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Space)){
+			_mainMenu.FadeOut();
+		}
+	}
+
+	public void SetDummyCameraActive(bool active){
+		_dummyCamera.gameObject.SetActive(active);
+	}
+}
